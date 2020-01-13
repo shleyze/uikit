1e57b7011ab00ab28dab006e5ba784757dd6c30d
vladimir.shleyze <vladimir.shleyze@instamart.ru>
2020-01-13 17:19:17 +0300

    Add storybook and "HelloWorld" component

---
+++ src/components/HelloWorld/HelloWorld.story.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { HelloWorld } from "../../index";
+
+export default {
+  title: "Components|HelloWorld",
+  component: HelloWorld,
+  parameters: {}
+};
+
+export const base = () => <HelloWorld />;
